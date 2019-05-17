93e1ce37236bf539759763878827ffbb55864c81
Sébastien VALLET <sebastien.vallet89@gmail.com>
2019-05-17 17:02:04 +0200

    first test

---
+++ chatbot/Test/TestChatMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using chatbot.Model;
+using NUnit.Framework;
+
+[TestFixture]
+public class TestChatMessage
+{
+	public TestChatMessage()
+	{
+        Bot bot1 = new Bot();
+        string myMessage = "Message de test 1";
+        ChatMessage chatMessage1 = new ChatMessage(bot1, myMessage);
+	}
+
+    [Test]
+    public void testSatanize()
+    {
+        string msg = "coucou... !'";
+        string msgExpected = "coucou... !'";
+        Assert.IsFalse(msg == msgExpected);
+    }
+}
