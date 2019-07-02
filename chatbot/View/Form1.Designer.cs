41401e6fe7db118b91aa059d3fed88aeeb0bc11a
ANTHONY FIGUEIREDO <anthony.figueiredo@viacesi.fr>
2019-07-02 14:08:33 +0200

    MAJ white space

--- chatbot/View/Form1.Designer.cs
+++ chatbot/View/Form1.Designer.cs
@@ -54,7 +54,7 @@
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(12, 640);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(445, 34);
+            this.textBox1.Size = new System.Drawing.Size(445, 28);
             this.textBox1.TabIndex = 2;
             // 
             // button1
@@ -83,6 +83,7 @@
             this.richTextBox1.Size = new System.Drawing.Size(522, 623);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
             // 
             // Form1
             // 
