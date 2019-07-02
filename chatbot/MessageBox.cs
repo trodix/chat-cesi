cffc7c6315829eadb7bfc5aa71224e12220b56f5
Aurelien Thazet <thazet.aurelien@gmail.com>
2019-07-02 16:33:32 +0200

    Graphic interface done

--- chatbot/MessageBox.cs
+++ chatbot/MessageBox.cs
@@ -12,9 +12,42 @@ namespace chatbot
 {
     public partial class MessageBox : UserControl
     {
+        public Image picUser = Image.FromFile("A:/GoogleDrive/2 - CESi/Projet/Chatbot/chat-cesi/chatbot/Assets/user1.png");
+        public Image picBot = Image.FromFile("A:/GoogleDrive/2 - CESi/Projet/Chatbot/chat-cesi/chatbot/Assets/bot.png");
+
         public MessageBox()
         {
             InitializeComponent();
+        }
+
+        public MessageBox(string pMessage, string pUserType)
+        {
+            InitializeComponent();
+
+            if (pUserType == "bot")
+            {
+                pictureBox1.Image = picBot;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                //pictureBox1.Top = 0;
+                pictureBox1.Left = 0;
+                //richTextBox1.Top = 0;
+                richTextBox1.Left = 100;
+
+                richTextBox1.BackColor = Color.DarkViolet;
+            }
+            else if (pUserType == "human")
+            {
+                pictureBox1.Image = picUser;
+            }
+
+            richTextBox1.AppendText(pMessage);
+            richTextBox1.AppendText(Environment.NewLine);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
